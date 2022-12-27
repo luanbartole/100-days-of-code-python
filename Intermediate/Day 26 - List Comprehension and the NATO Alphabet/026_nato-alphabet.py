9d8450ad36fd7510df282cb0d9d1efd95ca30e8f
Luan Bartole <luan.bartole@outlook.com>
2022-12-27 19:40:55 -0200

    Create 026_nato-alphabet

--- Intermediate/Day 26 - List Comprehension and the NATO Alphabet/026_nato-alphabet.py
+++ Intermediate/Day 26 - List Comprehension and the NATO Alphabet/026_nato-alphabet.py
@@ -1,8 +1,9 @@
-# Open csv containing the data.
 import pandas
+
+# Open csv containing the data.
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# Dictionary comprehension based on iterating rows of the data frame called data.
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
 # List comprehension of the nato alphabet based on each letter of the name.
 name = input("Write a name: ").upper()
