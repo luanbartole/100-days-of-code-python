094a747cc25108b5ac30771bc88ae9c759ccdc47
Luan Bartole <luan.bartole@outlook.com>
2022-12-30 15:57:16 -0200

    Create 028_password-manager.py

--- Intermediate/Day 28 - Pomodoro GUI Application/028_pomodoro.py
+++ Intermediate/Day 28 - Pomodoro GUI Application/028_pomodoro.py
@@ -71,7 +71,7 @@ timer_label.grid(column=1, row=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="20:00", fill="white", font=(FONT_NAME, 30, "bold"))
+timer_text = canvas.create_text(100, 130, text="25:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
 
 start_button = Button(text="Start", bd=0, command=start_timer)
