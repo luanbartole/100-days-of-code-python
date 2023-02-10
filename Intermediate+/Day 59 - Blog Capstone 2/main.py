1cfe091a1df007a105fb358d2bef4047a1b4464c
Luan Bartole <luan.bartole@outlook.com>
2023-02-10 05:28:25 -0200

    blog-capstone-2

---
+++ Intermediate+/Day 59 - Blog Capstone 2/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from post import Post
+import requests
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+@app.route('/index.html')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
